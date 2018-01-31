0eb82209fefd76067f0a79f58d4749bde7f44809
Oscar Törnquist <tornquist93@gmail.com>
2018-01-31 13:38:11 +0100

    Up to date

--- 03 - Träd/Träd/src/alda/tree/BinarySearchTreeNode.java
+++ 03 - Träd/Träd/src/alda/tree/BinarySearchTreeNode.java
@@ -1,7 +1,6 @@
 package alda.tree;
 
 /**
- *
  * Detta är den enda av de tre klasserna ni ska göra några ändringar i. (Om ni
  * inte vill lägga till fler testfall.) De ändringar som är tillåtna är dock
  * begränsade av följande:
@@ -12,9 +11,8 @@ package alda.tree;
  * <li>Ni FÅR lägga till fler metoder, dessa ska då vara privata.
  * </ul>
  *
- * @author henrikbe
- *
  * @param <T>
+ * @author henrikbe
  */
 @SuppressWarnings("unused") // Denna rad ska plockas bort. Den finns här
 // tillfälligt för att vi inte ska tro att det är
@@ -31,6 +29,20 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
     }
 
     public boolean add(T data) {
+        if (data != null && data.compareTo(this.data) > 0) {
+            if (right == null) {
+            right = new BinarySearchTreeNode<>(data);
+            return true;
+            }
+            return right.add(data);
+        } else if (data != null && data.compareTo(this.data) < 0) {
+            if (left == null) {
+                left = new BinarySearchTreeNode<>(data);
+                return true;
+            }
+            return left.add(data);
+        }
+
         return false;
     }
 
@@ -43,18 +55,59 @@ public class BinarySearchTreeNode<T extends Comparable<T>> {
     }
 
     public boolean contains(T data) {
-        return false;
+       if (data == null) {
+           return false;
+       }
+
+       if (data.compareTo(this.data) == 0) {
+           return true;
+       } else if (right != null && data.compareTo(this.data) > 0) {
+           return right.contains(data);
+       } else if (left != null && data.compareTo(this.data) < 0) {
+           return left.contains(data);
+       }
+       return false;
     }
 
     public int size() {
-        return 0;
+        return size(this);
     }
 
+    private int size(BinarySearchTreeNode<T> n) {
+        if (n == null) {
+            return 0;
+        } else {
+            return size(n.left) + size(n.right) + 1;
+        }
+    }
     public int depth() {
-        return -1;
+        return depth(this) -1;
+    }
+
+    private int depth(BinarySearchTreeNode<T> n) {
+        if (n == null) {
+            return 0;
+
+        } else {
+            return Math.max(depth(n.left), depth(n.right)) + 1;
+        }
     }
 
     public String toString() {
+        StringBuilder str = new StringBuilder();
+        toString(str,this);
+        return str.toString();
+    }
+
+    private String toString(StringBuilder str, BinarySearchTreeNode<T> n) {
+        if (n == null) {
+            return "";
+        }
+        str.append(toString(str, n.left));
+        str.append(n.data);
+        str.append(toString(str, n.right));
         return "";
+
+
     }
 }
--- 03 - Träd/Träd/src/alda/tree/BinarySearchTreeTester.java
+++ 03 - Träd/Träd/src/alda/tree/BinarySearchTreeTester.java
@@ -18,7 +18,7 @@ public class BinarySearchTreeTester {
         assertEquals(-1, tree.depth());
         assertEquals("[]", tree.toString());
     }
-/*
+
 
     @Before
     public void setUp() throws Exception {
@@ -51,7 +51,7 @@ public class BinarySearchTreeTester {
     public void testToString() {
         assertEquals("[1, 2, 3, 4, 5, 6]", tree.toString());
     }
-
+/*
     @Test
     public void testAddDuplicates() {
         for (int n = 1; n <= 6; n += 2)
