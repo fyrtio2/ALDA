211d6df58e251e38d8b333ba6a16c8d357529506
Oscar Törnquist <tornquist93@gmail.com>
2018-02-24 16:22:30 +0100

    awd

--- 07 - Grafer/FinalProject/src/alda/graph/Node.java
+++ 07 - Grafer/FinalProject/src/alda/graph/Node.java
@@ -1,4 +1,22 @@
 package alda.graph;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Node<T> {
+    private T data;
+    private boolean visited;
+    private Set<Node<T>> neighbours = new HashSet<>();
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
 }
