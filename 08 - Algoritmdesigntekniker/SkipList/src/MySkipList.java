9b63e03ef176b6b864b397013e8d091334f5eabd
Oscar Törnquist <tornquist93@gmail.com>
2018-02-28 13:42:45 +0100

    test

--- 08 - Algoritmdesigntekniker/SkipList/src/MySkipList.java
+++ 08 - Algoritmdesigntekniker/SkipList/src/MySkipList.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *@Author Oscar Törnquist   -osta3589
  *@Author Emil Rosell       -emro9957
@@ -5,8 +8,11 @@
 
 public class MySkipList<T extends Comparable<T>> {
 
-    private class Node<T> {
+    private class Node<E> {
+        private E data;
+        private List<E> nextNodes = new LinkedList<>();
 
     }
+
 }
 
