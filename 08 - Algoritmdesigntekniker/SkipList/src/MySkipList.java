02da9e36bfa33f0fc736dad2c018287da41cde67
Oscar Törnquist <tornquist93@gmail.com>
2018-02-28 20:44:57 +0100

    lets do this

--- 08 - Algoritmdesigntekniker/SkipList/src/MySkipList.java
+++ 08 - Algoritmdesigntekniker/SkipList/src/MySkipList.java
@@ -4,13 +4,48 @@ import java.util.List;
 /**
  *@Author Oscar Törnquist   -osta3589
  *@Author Emil Rosell       -emro9957
+ *u
  */
 
 public class MySkipList<T extends Comparable<T>> {
 
-    private class Node<E> {
-        private E data;
-        private List<E> nextNodes = new LinkedList<>();
+    private int size;
+    private Node<T> head, tail;
+
+    public boolean add(T data) {
+        return addLast(data);
+    }
+
+    public boolean contains(T data) {
+        return find(data) != null;
+    }
+
+    public boolean addFirst(T data) {
+        return false;
+    }
+
+    public boolean remove(T data) {
+        return poll(data) != null;
+    }
+
+    public Node<T> poll(T data) {
+        return null;
+    }
+
+    public Node<T> getHead(){ return head; }
+    public Node<T> getTail() { return tail; }
+
+    private Node<T> find(T data) {
+        return null;
+    }
+
+    private boolean addLast(T data) {
+        return false;
+    }
+
+    private class Node<T> {
+        T data;
+        List<T> nextNodes = new LinkedList<>();
 
     }
 
